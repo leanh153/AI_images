a9a71718da2ee80daa5aab0375aac47f10c05d69
LeAnh <anhle1531994@gmail.com>
2020-04-06 20:47:44 +0700

    change file type

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ for i in *; do rename 's/ /-/g' "$i" && rename 's/jfif/jpg/g' "$i";done
 git add .
 read -p "commit message: " msg
 git commit -m "$msg"
-clip.exe < "b6cd1fb1f222e001709cbde5bd8c94155358b594"
+echo "b6cd1fb1f222e001709cbde5bd8c94155358b594" | clip.exe
 git push -u origin master
