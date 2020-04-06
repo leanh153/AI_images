43427ac9e7e0baaefb04732d1661fbe8f2bcabc1
LeAnh <anhle1531994@gmail.com>
2020-04-06 20:40:09 +0700

    change to jpg image type

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in *; do rename 's/ /-/g' "$i" && rename 's/jfif/jpg/g' "$i";done
 git add .
-git commit -m "add images"
+read -p "commit message: " msg
+git commit -m "$msg"
 clip.exe < "b6cd1fb1f222e001709cbde5bd8c94155358b594"
 git push -u origin master
