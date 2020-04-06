7a62c1d0ed27379f7f39a60f0cf97052dea37da6
LeAnh <anhle1531994@gmail.com>
2020-04-06 20:34:14 +0700

    add images

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in *; do rename 's/ /-/g' "$i";done
+for i in *; do rename 's/ /-/g' "$i" && rename 's/jfif/jpg/g' "$i";done
 git add .
 git commit -m "add images"
 clip.exe < "b6cd1fb1f222e001709cbde5bd8c94155358b594"
