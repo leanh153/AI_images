37910a542baea5d5fdc139790afa7f492c8b0030
LeAnh <anhle1531994@gmail.com>
2020-04-07 14:43:04 +0700

    add gradient descent picture

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in *; do rename 's/ /-/g' "$i" && rename 's/jfif/jpg/g' "$i";done
+for i in *; do rename 's/ /-/g' "$i";done
+for i in *; do rename 's/jfif/jpg/g' "$i";done
 git add .
 read -p "commit message: " msg
 git commit -m "$msg"
